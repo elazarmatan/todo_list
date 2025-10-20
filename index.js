52b4e37132dacba9fb4cacd8cf9ecad761479c69
elazar matan <elazarmata8@gmail.com>
2025-10-20 16:33:27 +0300

    Refactor task addition logic to use existing HTML structure and improve event handling

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var root = document.querySelector("#root");
-function addTask(element) {
-    var addMission = document.createElement("section");
-    element === null || element === void 0 ? void 0 : element.appendChild(addMission);
+var root = document.querySelector("main");
+function addTask(main) {
+    var addMission = document.querySelector("#addTask");
     var input = document.createElement("input");
     input.placeholder = "add";
     input.name = "add";
@@ -9,23 +8,36 @@ function addTask(element) {
     var create = document.createElement("button");
     create.textContent = "submit";
     addMission.appendChild(create);
-    var missions = document.createElement("ul");
-    element === null || element === void 0 ? void 0 : element.appendChild(missions);
+    var missions = document.querySelector("#tasks");
     create.addEventListener("click", function (e) {
         e.preventDefault();
-        var mission = document.createElement("li");
-        mission.innerHTML = input.value;
-        var removeTask = document.createElement('button');
-        removeTask.textContent = 'remove';
-        removeTask.addEventListener('click', function () {
-            var _a;
-            (_a = removeTask.parentElement) === null || _a === void 0 ? void 0 : _a.remove();
-        });
-        var done = document.createElement('input');
-        done.type = 'checkbox';
-        mission.appendChild(done);
-        mission.appendChild(removeTask);
-        missions.appendChild(mission);
+        if (input.value) {
+            var mission_1 = document.createElement("li");
+            mission_1.innerHTML = input.value;
+            var removeTask_1 = document.createElement("button");
+            removeTask_1.textContent = "remove";
+            removeTask_1.addEventListener("click", function () {
+                var _a;
+                (_a = removeTask_1.parentElement) === null || _a === void 0 ? void 0 : _a.remove();
+            });
+            var done = document.createElement("input");
+            done.addEventListener('change', function (e) {
+                var _a, _b;
+                if (((_a = mission_1.parentElement) === null || _a === void 0 ? void 0 : _a.id) === 'tasks') {
+                    var tasksDone = document.querySelector('#tasksDone');
+                    tasksDone === null || tasksDone === void 0 ? void 0 : tasksDone.appendChild(mission_1);
+                    // done.parentElement?.remove()
+                }
+                else if (((_b = mission_1.parentElement) === null || _b === void 0 ? void 0 : _b.id) === 'tasksDone') {
+                    missions === null || missions === void 0 ? void 0 : missions.appendChild(mission_1);
+                    // done.parentElement?.remove()
+                }
+            });
+            done.type = "checkbox";
+            mission_1.appendChild(done);
+            mission_1.appendChild(removeTask_1);
+            missions === null || missions === void 0 ? void 0 : missions.appendChild(mission_1);
+        }
     });
 }
 addTask(root);
--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
-const root = document.querySelector<HTMLElement>("#root");
-function addTask(element: HTMLElement | null) {
-  const addMission = document.createElement("section");
-  element?.appendChild(addMission);
+const root = document.querySelector<HTMLElement>("main");
+function addTask(main: HTMLElement | null) {
+  const addMission = document.querySelector("#addTask");
   const input = document.createElement("input");
   input.placeholder = "add";
   input.name = "add";
@@ -9,22 +8,34 @@ function addTask(element: HTMLElement | null) {
   const create = document.createElement("button");
   create.textContent = "submit";
   addMission.appendChild(create);
-  const missions = document.createElement("ul");
-  element?.appendChild(missions);
+  const missions = document.querySelector("#tasks");
   create.addEventListener("click", (e) => {
     e.preventDefault();
-    const mission = document.createElement("li");
-    mission.innerHTML = input.value;
-    const removeTask = document.createElement('button')
-    removeTask.textContent = 'remove'
-    removeTask.addEventListener('click',() => {
-        removeTask.parentElement?.remove()
-    })
-    const done = document.createElement('input')
-    done.type = 'checkbox'
-    mission.appendChild(done)
-    mission.appendChild(removeTask)
-    missions.appendChild(mission);
+    if (input.value) {
+      const mission = document.createElement("li");
+      mission.innerHTML = input.value;
+      const removeTask = document.createElement("button");
+      removeTask.textContent = "remove";
+      removeTask.addEventListener("click", () => {
+        removeTask.parentElement?.remove();
+      });
+      const done = document.createElement("input");
+      done.addEventListener('change',e => {
+        if(mission.parentElement?.id === 'tasks'){
+            const tasksDone = document.querySelector('#tasksDone')
+            tasksDone?.appendChild(mission)
+            // done.parentElement?.remove()
+        }
+        else if(mission.parentElement?.id === 'tasksDone'){
+            missions?.appendChild(mission)
+            // done.parentElement?.remove()
+        }
+      })
+      done.type = "checkbox";
+      mission.appendChild(done);
+      mission.appendChild(removeTask);
+      missions?.appendChild(mission);
+    }
   });
 }
-addTask(root)
+addTask(root);
