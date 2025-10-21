5e521f85014b6a14e8671a10ef4075dae1f7697e
elazar matan <elazarmata8@gmail.com>
2025-10-21 09:17:40 +0300

    Refactor task addition logic to handle input submission via Enter key

--- index.ts
+++ index.ts
@@ -5,36 +5,34 @@ function addTask(main: HTMLElement | null) {
   input.placeholder = "add";
   input.name = "add";
   addMission?.appendChild(input);
-  const create = document.createElement("button");
-  create.textContent = "submit";
-  addMission.appendChild(create);
   const missions = document.querySelector("#tasks");
-  create.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (input.value) {
-      const mission = document.createElement("li");
-      mission.innerHTML = input.value;
-      const removeTask = document.createElement("button");
-      removeTask.textContent = "remove";
-      removeTask.addEventListener("click", () => {
-        removeTask.parentElement?.remove();
-      });
-      const done = document.createElement("input");
-      done.addEventListener('change',e => {
-        if(mission.parentElement?.id === 'tasks'){
-            const tasksDone = document.querySelector('#tasksDone')
-            tasksDone?.appendChild(mission)
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      if (input.value) {
+        const mission = document.createElement("li");
+        mission.innerHTML = input.value;
+        const removeTask = document.createElement("button");
+        removeTask.textContent = "remove";
+        removeTask.addEventListener("click", () => {
+          removeTask.parentElement?.remove();
+        });
+        const done = document.createElement("input");
+        done.addEventListener("change", (e) => {
+          if (mission.parentElement?.id === "tasks") {
+            const tasksDone = document.querySelector("#tasksDone");
+            tasksDone?.appendChild(mission);
             // done.parentElement?.remove()
-        }
-        else if(mission.parentElement?.id === 'tasksDone'){
-            missions?.appendChild(mission)
+          } else if (mission.parentElement?.id === "tasksDone") {
+            missions?.appendChild(mission);
             // done.parentElement?.remove()
-        }
-      })
-      done.type = "checkbox";
-      mission.appendChild(done);
-      mission.appendChild(removeTask);
-      missions?.appendChild(mission);
+          }
+        });
+        done.type = "checkbox";
+        mission.appendChild(done);
+        mission.appendChild(removeTask);
+        missions?.appendChild(mission);
+      }
+      input.value = ''
     }
   });
 }
