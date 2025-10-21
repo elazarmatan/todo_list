56710745fe08f759a920a86991a34dc747be0998
elazar matan <elazarmata8@gmail.com>
2025-10-21 17:40:04 +0300

    Refactor task management functions to enhance task creation, editing, and retrieval logic

--- index.js
+++ index.js
@@ -1,42 +1,98 @@
-var root = document.querySelector("main");
-function addTask(main) {
+function createTask(task) {
+    var _a, _b;
+    var finishTask = { 'task': task, id: 0 };
+    var dataExist;
+    try {
+        dataExist = JSON.parse((_a = localStorage === null || localStorage === void 0 ? void 0 : localStorage.getItem('tasks')) !== null && _a !== void 0 ? _a : '[]');
+    }
+    catch (error) {
+        dataExist = [];
+    }
+    finishTask.id = ((_b = dataExist[dataExist.length - 1]) === null || _b === void 0 ? void 0 : _b.id) + 1;
+    dataExist.unshift(finishTask);
+    localStorage.setItem('tasks', JSON.stringify(dataExist));
+}
+function updateTask() { }
+function deleteTask() { }
+function getAllTasks() {
+    var _a;
+    var allTasks = JSON.parse((_a = localStorage === null || localStorage === void 0 ? void 0 : localStorage.getItem('tasks')) !== null && _a !== void 0 ? _a : '[]');
+    var missions = document.querySelector("#tasks");
+    console.log("type:".concat(typeof allTasks));
+    allTasks.forEach(function (taskObj) { return task(taskObj.task, missions); });
+}
+function task(task, container) {
+    var mission = document.createElement("li");
+    var taskP = document.createElement('p');
+    taskP.innerText = task;
+    var removeTask = document.createElement("button");
+    removeTask.textContent = "remove";
+    removeTask.addEventListener("click", function () {
+        var _a;
+        (_a = removeTask.parentElement) === null || _a === void 0 ? void 0 : _a.remove();
+    });
+    var labelDone = document.createElement('label');
+    labelDone.htmlFor = 'done';
+    labelDone.textContent = 'done';
+    var checkDone = document.createElement("input");
+    checkDone.id = 'done';
+    checkDone.addEventListener("change", function (e) {
+        var _a, _b;
+        if (((_a = mission.parentElement) === null || _a === void 0 ? void 0 : _a.id) === "tasks") {
+            labelDone.textContent = 'not done';
+            mission.removeChild(edit);
+            var tasksDone = document.querySelector("#tasksDone");
+            tasksDone === null || tasksDone === void 0 ? void 0 : tasksDone.appendChild(mission);
+        }
+        else if (((_b = mission.parentElement) === null || _b === void 0 ? void 0 : _b.id) === "tasksDone") {
+            labelDone.textContent = 'done';
+            mission.appendChild(edit);
+            container === null || container === void 0 ? void 0 : container.appendChild(mission);
+        }
+    });
+    checkDone.type = "checkbox";
+    var edit = document.createElement('button');
+    edit.textContent = 'edit';
+    edit.addEventListener('click', function () {
+        taskP.setAttribute('contenteditable', 'true');
+        taskP.focus();
+    });
+    var originalText;
+    taskP.addEventListener('focus', function () {
+        originalText = taskP.textContent;
+    });
+    taskP.addEventListener('keydown', function (e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            taskP.setAttribute('contenteditable', 'false');
+        }
+        if (e.key === 'Escape') {
+            e.preventDefault();
+            taskP.textContent = originalText;
+            taskP.setAttribute('contenteditable', 'false');
+        }
+    });
+    var done = document.createElement('div');
+    done.append(checkDone, labelDone);
+    mission.append(done, taskP, removeTask, edit);
+    container === null || container === void 0 ? void 0 : container.appendChild(mission);
+}
+function addTask() {
     var addMission = document.querySelector("#addTask");
     var input = document.createElement("input");
-    input.placeholder = "add";
+    input.placeholder = "add task";
     input.name = "add";
+    input.id = 'addInp';
     addMission === null || addMission === void 0 ? void 0 : addMission.appendChild(input);
-    var missions = document.querySelector("#tasks");
     input.addEventListener("keydown", function (e) {
         if (e.key === "Enter") {
             if (input.value) {
-                var mission_1 = document.createElement("li");
-                mission_1.innerHTML = input.value;
-                var removeTask_1 = document.createElement("button");
-                removeTask_1.textContent = "remove";
-                removeTask_1.addEventListener("click", function () {
-                    var _a;
-                    (_a = removeTask_1.parentElement) === null || _a === void 0 ? void 0 : _a.remove();
-                });
-                var done = document.createElement("input");
-                done.addEventListener("change", function (e) {
-                    var _a, _b;
-                    if (((_a = mission_1.parentElement) === null || _a === void 0 ? void 0 : _a.id) === "tasks") {
-                        var tasksDone = document.querySelector("#tasksDone");
-                        tasksDone === null || tasksDone === void 0 ? void 0 : tasksDone.appendChild(mission_1);
-                        // done.parentElement?.remove()
-                    }
-                    else if (((_b = mission_1.parentElement) === null || _b === void 0 ? void 0 : _b.id) === "tasksDone") {
-                        missions === null || missions === void 0 ? void 0 : missions.appendChild(mission_1);
-                        // done.parentElement?.remove()
-                    }
-                });
-                done.type = "checkbox";
-                mission_1.appendChild(done);
-                mission_1.appendChild(removeTask_1);
-                missions === null || missions === void 0 ? void 0 : missions.appendChild(mission_1);
+                createTask(input.value);
+                getAllTasks();
             }
             input.value = '';
         }
     });
 }
-addTask(root);
+getAllTasks();
+addTask();
--- index.ts
+++ index.ts
@@ -1,39 +1,93 @@
-const root = document.querySelector<HTMLElement>("main");
-function addTask(main: HTMLElement | null) {
-  const addMission = document.querySelector("#addTask");
-  const input = document.createElement("input");
-  input.placeholder = "add";
-  input.name = "add";
-  addMission?.appendChild(input);
-  const missions = document.querySelector("#tasks");
-  input.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      if (input.value) {
+function createTask(task:string){
+    const finishTask = {'task':task,id :0}
+    let dataExist
+    try {
+        dataExist= JSON.parse(localStorage?.getItem('tasks') ?? '[]')
+    } catch (error) {
+        dataExist = []
+    }      
+    finishTask.id = dataExist[dataExist.length - 1]?.id + 1
+    dataExist.unshift(finishTask)
+    localStorage.setItem('tasks',JSON.stringify(dataExist))
+}
+function updateTask(){}
+function deleteTask(){}
+function getAllTasks(){
+    const allTasks= JSON.parse(localStorage?.getItem('tasks') ?? '[]')
+    const missions = document.querySelector("#tasks");
+    console.log(`type:${typeof allTasks}`)
+    allTasks.forEach(taskObj => task(taskObj.task,missions))
+}
+function task(task:string,container:Element|null){
         const mission = document.createElement("li");
-        mission.innerHTML = input.value;
+        const taskP = document.createElement('p')
+        taskP.innerText = task
         const removeTask = document.createElement("button");
         removeTask.textContent = "remove";
         removeTask.addEventListener("click", () => {
           removeTask.parentElement?.remove();
         });
-        const done = document.createElement("input");
-        done.addEventListener("change", (e) => {
+         const labelDone = document.createElement('label')
+        labelDone.htmlFor = 'done'
+        labelDone.textContent = 'done'
+        const checkDone = document.createElement("input");
+        checkDone.id = 'done'
+        checkDone.addEventListener("change", (e) => {
           if (mission.parentElement?.id === "tasks") {
+            labelDone.textContent = 'not done'
+            mission.removeChild(edit)
             const tasksDone = document.querySelector("#tasksDone");
             tasksDone?.appendChild(mission);
-            // done.parentElement?.remove()
           } else if (mission.parentElement?.id === "tasksDone") {
-            missions?.appendChild(mission);
-            // done.parentElement?.remove()
+            labelDone.textContent = 'done'
+            mission.appendChild(edit)
+            container?.appendChild(mission);
           }
-        });
-        done.type = "checkbox";
-        mission.appendChild(done);
-        mission.appendChild(removeTask);
-        missions?.appendChild(mission);
-      }
+          });
+        checkDone.type = "checkbox";
+        const edit = document.createElement('button')
+        edit.textContent = 'edit'
+        edit.addEventListener('click',() => {
+            taskP.setAttribute('contenteditable','true')
+            taskP.focus()
+        })
+        let originalText:string | null
+        taskP.addEventListener('focus',() => {
+            originalText = taskP.textContent
+        })
+        taskP.addEventListener('keydown',e => {
+            if(e.key === 'Enter'){
+                e.preventDefault()
+                taskP.setAttribute('contenteditable','false')
+            }
+            if(e.key === 'Escape'){
+                e.preventDefault()
+                taskP.textContent = originalText
+                taskP.setAttribute('contenteditable','false')
+            }
+        })
+        const done = document.createElement('div')
+        done.append(checkDone,labelDone)
+        mission.append(done,taskP,removeTask,edit)
+        container?.appendChild(mission);  
+}
+
+function addTask() {
+  const addMission = document.querySelector("#addTask");
+  const input = document.createElement("input");
+  input.placeholder = "add task";
+  input.name = "add";
+  input.id = 'addInp'
+  addMission?.appendChild(input);
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      if (input.value) {
+        createTask(input.value)
+        getAllTasks()
+      } 
       input.value = ''
     }
   });
 }
-addTask(root);
+getAllTasks()
+addTask();
