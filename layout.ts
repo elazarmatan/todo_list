e80b71a601effd899c7173cd3aa2b1efd6f7d781
elazar matan <elazarmata8@gmail.com>
2025-10-21 09:17:52 +0300

    Refactor task list structure to create and append titles for 'TO DO' and 'DONE' sections

--- layout.ts
+++ layout.ts
@@ -8,11 +8,14 @@ const addTasks = document.createElement('section')
 addTasks.id = 'addTask'
 const tasks = document.createElement('ul')
 tasks.id = 'tasks'
+const tasksTitle = document.createElement('h2')
+tasksTitle.innerText = 'TO DO'
+tasks.appendChild(tasksTitle)
 const tasksDone = document.createElement('ul')
 tasksDone.id = 'tasksDone'
-const title = document.createElement('h2')
-title.innerText = 'DONE'
-tasksDone.appendChild(title)
+const tasksDoneTitle = document.createElement('h2')
+tasksDoneTitle.innerText = 'DONE'
+tasksDone.appendChild(tasksDoneTitle)
 main.appendChild(addTasks)
 main?.appendChild(tasks)
 main?.appendChild(tasksDone)
