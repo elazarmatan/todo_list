8eb12e3f0c8a2ccc490ca9d298fea0be50b674f5
elazar matan <elazarmata8@gmail.com>
2025-10-21 17:39:55 +0300

    Refactor layout function to streamline DOM element creation and appending

--- layout.ts
+++ layout.ts
@@ -1,21 +1,19 @@
-const root = document.querySelector<HTMLElement>('#root')
-const header = document.createElement('h1')
-header.innerText = 'TODO LIST'
-root?.appendChild(header)
-const main = document.createElement('main')
-root?.appendChild(main)
-const addTasks = document.createElement('section')
-addTasks.id = 'addTask'
-const tasks = document.createElement('ul')
-tasks.id = 'tasks'
-const tasksTitle = document.createElement('h2')
-tasksTitle.innerText = 'TO DO'
-tasks.appendChild(tasksTitle)
-const tasksDone = document.createElement('ul')
-tasksDone.id = 'tasksDone'
-const tasksDoneTitle = document.createElement('h2')
-tasksDoneTitle.innerText = 'DONE'
-tasksDone.appendChild(tasksDoneTitle)
-main.appendChild(addTasks)
-main?.appendChild(tasks)
-main?.appendChild(tasksDone)
+function layout() {
+  const root = document.querySelector<HTMLElement>("#root");
+  const header = document.createElement("h1");
+  header.innerText = "TODO LIST";
+  const main = document.createElement("main");
+  const addTasks = document.createElement("section");
+  addTasks.id = "addTask";
+  const tasks = document.createElement("ul");
+  tasks.id = "tasks";
+  const tasksTitle = document.createElement("h2");
+  tasksTitle.innerText = "TO DO";
+  const tasksDone = document.createElement("ul");
+  tasksDone.id = "tasksDone";
+  const tasksDoneTitle = document.createElement("h2");
+  tasksDoneTitle.innerText = "DONE";
+  main.append(addTasks,tasksTitle, tasks,tasksDoneTitle, tasksDone);
+  root?.append(header,main)
+}
+layout();
