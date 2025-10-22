30f8933652cafb06ddcd0457a28a2f2da9dfab80
elazar matan <elazarmata8@gmail.com>
2025-10-22 15:26:23 +0300

    Add navigation bar with delete buttons to layout

--- layout.ts
+++ layout.ts
@@ -2,6 +2,15 @@ function layout() {
   const root = document.querySelector<HTMLElement>("#root");
   const header = document.createElement("h1");
   header.innerText = "TODO LIST";
+  const navBar = document.createElement('div')
+  navBar.id = 'navBar'
+  const deleteAll = document.createElement('button')
+  const deleteDone = document.createElement('button')
+  deleteAll.textContent = 'Delete All'
+  deleteDone.textContent = 'Delete Done'
+  deleteAll.id = 'DeleteAll'
+  deleteDone.id = 'DeleteDone'
+  navBar.append(deleteAll,deleteDone)
   const main = document.createElement("main");
   const addTasks = document.createElement("section");
   addTasks.id = "addTask";
@@ -14,6 +23,6 @@ function layout() {
   const tasksDoneTitle = document.createElement("h2");
   tasksDoneTitle.innerText = "DONE";
   main.append(addTasks,tasksTitle, tasks,tasksDoneTitle, tasksDone);
-  root?.append(header,main)
+  root?.append(header,navBar,main)
 }
 layout();
